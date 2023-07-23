d4170ee0faf5b7a5a6d65d15ef533b41331ec0ca
Stepan Skichko <st.skichko@gmail.com>
2023-07-23 10:16:06 +0300

    delete subscription

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,7 +2,6 @@ const register = require('./register');
 const login = require('./login');
 const logout = require('./logout');
 const getCurrent = require('./getCurrent');
-const changeSubscription = require('./changeSubscription');
 const { controllerWrapper } = require('../../helpers');
 
 module.exports = {
@@ -10,5 +9,4 @@ module.exports = {
   login: controllerWrapper(login),
   logout: controllerWrapper(logout),
   getCurrent: controllerWrapper(getCurrent),
-  changeSubscription: controllerWrapper(changeSubscription),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { validateBody, authenticate } = require('../../middlewares');
 const controller = require('../../controllers/auth');
-const { registerSchema, loginSchema, changeSubscriptionSchema } = require('../../models/user');
+const { registerSchema, loginSchema } = require('../../models/user');
 const router = express.Router();
 
 // signup
@@ -15,13 +15,5 @@ router.post('/logout', authenticate, controller.logout);
 
 // current
 router.get('/current', authenticate, controller.getCurrent);
-
-// update Subscription
-router.patch(
-  '/',
-  authenticate,
-  validateBody(changeSubscriptionSchema),
-  controller.changeSubscription
-);
 
 module.exports = router;
